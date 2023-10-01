93ac696f81d51ae8f467073f9c373f6ed2cbebe1
Dachrono <andres_zamorano_@hotmail.com>
2023-10-01 17:46:14 -0600

    Creacion de relaciones y creacion de tabla en archivo migrate

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,2 +1,3 @@
 class Cliente < ApplicationRecord
+    has_many :mascota, class_name: "Mascota", foreign_key: "cliente_id"
 end
--- db/migrate/20231001212724_create_clientes.rb
+++ db/migrate/20231001212724_create_clientes.rb
@@ -1,10 +1,10 @@
 class CreateClientes < ActiveRecord::Migration[7.0]
   def change
     create_table :clientes do |t|
-      t.string :nombre
-      t.text :direccion
-      t.string :email
-      t.integer :telefono
+      t.string :nombre, null: false
+      t.text :direccion, default: "Sin dirección" 
+      t.string :email, default: "Sin email" 
+      t.integer :telefono, null: false 
 
       t.timestamps
     end
