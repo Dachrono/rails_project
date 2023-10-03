da30142cfd87aafe6ee881c197f20176c17e5fe5
Dachrono <andres_zamorano_@hotmail.com>
2023-10-03 14:12:50 -0600

    Creacion de metodos para ingrear cliente nuevo

--- app/controllers/clientes_controller.rb
+++ app/controllers/clientes_controller.rb
@@ -6,4 +6,25 @@ class ClientesController < ApplicationController
     def details
         @cliente = Cliente.find(params[:id])
     end
+
+    def new
+        @nuevo = Cliente.new # Crea un objeto vacío
+    end
+    
+    def create
+      @nuevo = Cliente.new(cliente_params)
+        if @nuevo.save
+            redirect_to clientes_path
+        else
+            render :new
+        end        
+    end
+    
+    private
+
+    def cliente_params
+        params.require(:cliente).permit(:nombre, :direccion, :email, :telefono)
+        # :cliente busca el objeto en el post y lo retorna
+        # .permit() define los campos que se pueden modificar o llamar para mandar a la base de datos
+    end
 end
