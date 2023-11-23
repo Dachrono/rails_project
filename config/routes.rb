c038a87de61c59578f8ae9c4234d2d9956bd9e0f
Dachrono <andres_zamorano_@hotmail.com>
2023-11-22 23:41:23 -0600

    Create style for the login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
   # root "articles#index"
+  devise_for :users
+  
   root "menu#index"
 
   get "/menu", to: "menu#index"
